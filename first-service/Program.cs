85e127e5f56fe86fa7b837818a02cc1b9f056cf1
Ritesh Agrawal <ritesh2303.agrawal@outlook.com>
2025-09-14 08:05:16 +0530

    change

--- first-service/Program.cs
+++ first-service/Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Spring", "Summer", "Autumn", "Winter"
 };
 
 app.MapGet("/weatherforecast", () =>
